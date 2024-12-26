55c3d428a169b556b4a60006b9ab1989d7302cd8
Srimanth <109213942+AISpaceXDragon@users.noreply.github.com>
2024-12-26 16:37:44 +0530

    Update eval_GOT_ocr.py

--- GOT-OCR-2.0-master/GOT/eval/eval_GOT_ocr.py
+++ GOT-OCR-2.0-master/GOT/eval/eval_GOT_ocr.py
@@ -31,7 +31,7 @@ from io import BytesIO
 from GOT.model.plug.blip_process import BlipImageEvalProcessor
 
 from transformers import TextStreamer
-from GOT.model.plug.transforms import train_transform, test_transform
+#from GOT.model.plug.transforms import train_transform, test_transform
 import re
 from GOT.demo.process_results import punctuation_dict, svg_to_html
 
